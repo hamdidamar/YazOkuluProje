9432361974ef76eda477556f1bb0de655e9d204d
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-04 17:23:38 +0300

    Add files via upload

---
+++ YazOkulu.BusinessLogicLayer/BLLOgrenci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YazOkulu.DataAccessLayer;
+using YazOkulu.Entity;
+
+namespace YazOkulu.BusinessLogicLayer
+{
+   public class BLLOgrenci
+    {
+        public static int OgrenciEkle(EntityOgrenci ogrenci)
+        {
+            if (ogrenci.OgrenciAd != null && ogrenci.OgrenciSoyad != null && ogrenci.OgrenciNumara != null && ogrenci.OgrenciMail != null && ogrenci.OgrenciSifre != null && ogrenci.OgrenciFotograf != null && ogrenci.OgrenciBakiye != null)
+            {
+                return DALOgrenci.OgrenciEkle(ogrenci);
+            }
+            return -1;
+        }
+    }
+}
