eb4a615d37a9b4ca3b289515a093bd4096c44ad8
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-04 17:24:10 +0300

    Add files via upload

---
+++ YazOkulu.DataAccessLayer/DALOgrenci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using YazOkulu.Entity;
+
+namespace YazOkulu.DataAccessLayer
+{
+   public class DALOgrenci
+    {
+       
+        public static int OgrenciEkle(EntityOgrenci ogrenci)
+        {
+            SqlCommand cmd = new SqlCommand("EXEC OgrenciEkle @p1 = @p1,@p2 = @p2,@p3 = @p3,@p4 = @p4,@p5 = @p5,@p6 = @p6,@p7 = @p7", DBHelper.bgl);
+            if (cmd.Connection.State != ConnectionState.Open)
+            {
+                cmd.Connection.Open();
+            }
+            Console.WriteLine(ogrenci.OgrenciAd);
+            cmd.Parameters.AddWithValue("@p1", ogrenci.OgrenciAd);
+            cmd.Parameters.AddWithValue("@p2", ogrenci.OgrenciSoyad);
+            cmd.Parameters.AddWithValue("@p3", ogrenci.OgrenciNumara);
+            cmd.Parameters.AddWithValue("@p4", ogrenci.OgrenciMail);
+            cmd.Parameters.AddWithValue("@p5", ogrenci.OgrenciSifre);
+            cmd.Parameters.AddWithValue("@p6", ogrenci.OgrenciFotograf);
+            cmd.Parameters.AddWithValue("@p7", ogrenci.OgrenciBakiye);
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ YazOkulu.DataAccessLayer/DBHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace YazOkulu.DataAccessLayer
+{
+    public class DBHelper
+    {
+        public static SqlConnection bgl = new SqlConnection(@"Data Source=HAMDIDAMAR\SQL_2014;Initial Catalog=DboYazOkulu;Integrated Security=True");    
+    }
+}
