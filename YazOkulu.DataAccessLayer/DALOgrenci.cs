f3f71614c121c416d5f726f4e7f13c756dc7bfad
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-05 00:20:55 +0300

    Update DALOgrenci.cs

--- YazOkulu.DataAccessLayer/DALOgrenci.cs
+++ YazOkulu.DataAccessLayer/DALOgrenci.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +19,6 @@ namespace YazOkulu.DataAccessLayer
             {
                 cmd.Connection.Open();
             }
-            Console.WriteLine(ogrenci.OgrenciAd);
             cmd.Parameters.AddWithValue("@p1", ogrenci.OgrenciAd);
             cmd.Parameters.AddWithValue("@p2", ogrenci.OgrenciSoyad);
             cmd.Parameters.AddWithValue("@p3", ogrenci.OgrenciNumara);
@@ -28,6 +27,43 @@ namespace YazOkulu.DataAccessLayer
             cmd.Parameters.AddWithValue("@p6", ogrenci.OgrenciFotograf);
             cmd.Parameters.AddWithValue("@p7", ogrenci.OgrenciBakiye);
             return cmd.ExecuteNonQuery();
+        }
+        public static List<EntityOgrenci> OgrenciListesi()
+        {
+            List<EntityOgrenci> degerler = new List<EntityOgrenci>();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TBL_OGRENCILER", DBHelper.bgl);
+            if (cmd.Connection.State != ConnectionState.Open)
+            {
+                cmd.Connection.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                EntityOgrenci ogrenci = new EntityOgrenci();
+                ogrenci.OgrenciId = Convert.ToInt32(dr["OgrenciID"].ToString());
+                ogrenci.OgrenciAd = dr["OgrenciAd"].ToString();
+                ogrenci.OgrenciSoyad = dr["OgrenciSoyad"].ToString();
+                ogrenci.OgrenciNumara = dr["OgrenciNumara"].ToString();
+                ogrenci.OgrenciMail = dr["OgrenciMail"].ToString();
+                ogrenci.OgrenciSifre = dr["OgrenciSifre"].ToString();
+                ogrenci.OgrenciFotograf = dr["OgrenciFotograf"].ToString();
+                ogrenci.OgrenciBakiye = Convert.ToDouble(dr["OgrenciBakiye"].ToString());
+
+                degerler.Add(ogrenci);
+            }
+            dr.Close();
+            return degerler;
+        }
+        public static bool OgrenciSil(int OgrenciID)
+        {
+            SqlCommand cmd = new SqlCommand("DELETE FROM TBL_OGRENCILER WHERE OgrenciID = @p1", DBHelper.bgl);
+            if (cmd.Connection.State != ConnectionState.Open)
+            {
+                cmd.Connection.Open();
+            }
+            cmd.Parameters.AddWithValue("@p1", OgrenciID);
+            return cmd.ExecuteNonQuery() > 0;
+
         }
     }
 }
