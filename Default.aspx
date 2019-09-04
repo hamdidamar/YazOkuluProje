e03d9e35fdec8754ff13dc01afd92ea1e6a598ca
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-04 17:25:03 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <form id ="Form1" runat ="server">
+        <asp:Button ID ="Btn_OgrenciEkle"  runat="server" text="Öğrenci Ekle" OnClick="Btn_OgrenciEkle_Click" />
+    </form>
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using YazOkulu.BusinessLogicLayer;
+using YazOkulu.DataAccessLayer;
+using YazOkulu.Entity;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    EntityOgrenci entityogrenci = new EntityOgrenci();
+    protected void Btn_OgrenciEkle_Click(object sender, EventArgs e)
+    {
+        
+        entityogrenci.OgrenciAd = "Hasan";
+        entityogrenci.OgrenciSoyad = "Kulmaz";
+        entityogrenci.OgrenciNumara = "968574123";
+        entityogrenci.OgrenciMail = "hasan.kulmaz@hotmail.com";
+        entityogrenci.OgrenciSifre = "456987";
+        entityogrenci.OgrenciFotograf = "FotoYol";
+        entityogrenci.OgrenciBakiye = 150;
+        BLLOgrenci.OgrenciEkle(entityogrenci);
+
+    }
+}
