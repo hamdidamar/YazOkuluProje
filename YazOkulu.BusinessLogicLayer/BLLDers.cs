60833924859c111bc88feaa639898bcf9836839f
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-06 00:01:55 +0300

    Add files via upload

---
+++ YazOkulu.BusinessLogicLayer/BLLDers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YazOkulu.DataAccessLayer;
+using YazOkulu.Entity;
+
+namespace YazOkulu.BusinessLogicLayer
+{
+    public class BLLDers
+    {
+        public static List<EntityDers> DersListesi()
+        {
+            return DALDers.DersListesi();
+        }
+        public static int DersTalepEkle(EntityBasvuru basvuru)
+        {
+            if (basvuru.BasvuruOgrenciId >= 0 && basvuru.BasvuruDersId >= 0)
+            {
+                return DALDers.DersTalepEkle(basvuru);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
--- YazOkulu.BusinessLogicLayer/BLLOgrenci.cs
+++ YazOkulu.BusinessLogicLayer/BLLOgrenci.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,23 +12,45 @@ namespace YazOkulu.BusinessLogicLayer
     {
         public static int OgrenciEkle(EntityOgrenci ogrenci)
         {
-            if (ogrenci.OgrenciAd != null && ogrenci.OgrenciSoyad != null && ogrenci.OgrenciNumara != null && ogrenci.OgrenciMail != null && ogrenci.OgrenciSifre != null && ogrenci.OgrenciFotograf != null && ogrenci.OgrenciBakiye != null)
+            if (ogrenci.OgrenciAd != null && ogrenci.OgrenciSoyad != null && ogrenci.OgrenciNumara != null && ogrenci.OgrenciMail != null && ogrenci.OgrenciSifre != null && ogrenci.OgrenciFotograf != null && ogrenci.OgrenciBakiye >=0)
             {
                 return DALOgrenci.OgrenciEkle(ogrenci);
             }
             return -1;
         }
+
         public static List<EntityOgrenci> OgrenciListesi()
         {
             return DALOgrenci.OgrenciListesi();
         }
+
         public static bool OgrenciSil(int OgrenciID)
         {
-            if (OgrenciID != null)
+            if (OgrenciID >= 0)
             {
                 return DALOgrenci.OgrenciSil(OgrenciID);
             }
-            return false;
+            else
+            {
+                return false;
+            }
+        }
+
+        public static List<EntityOgrenci> OgrenciDetay(int OgrenciID)
+        {
+            return DALOgrenci.OgrenciDetay(OgrenciID);
+        }
+
+        public static bool OgrenciGuncelle(EntityOgrenci ogrenci)
+        {
+            if (ogrenci.OgrenciAd != null && ogrenci.OgrenciSoyad != null && ogrenci.OgrenciNumara != null && ogrenci.OgrenciId > 0)
+            {
+                return DALOgrenci.OgrenciGuncelle(ogrenci);
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
